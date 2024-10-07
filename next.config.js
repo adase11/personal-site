05e1d6ee33adce87241c195953dec60c5578a7c3
Austin Dase <59173272+adase11@users.noreply.github.com>
2024-10-07 13:26:36 -0400

    workflow

---
+++ next.config.js
@@ -0,0 +1,15 @@
+// next.config.js
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true,
+    }
+    // Uncomment and set these to your actual base path and asset prefix if needed for GitHub Pages or other hosting
+    // basePath: '/your-repo-name',
+    // assetPrefix: '/your-repo-name/',
+  };
+  
+  export default nextConfig;
