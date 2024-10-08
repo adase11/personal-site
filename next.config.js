497fef5496203d690f4ab6e8ce5cb2833c12ca67
Austin Dase <59173272+adase11@users.noreply.github.com>
2024-10-07 20:10:45 -0400

    removing trailing slashes

--- next.config.js
+++ next.config.js
@@ -6,8 +6,7 @@ const nextConfig = {
     output: 'export',
     images: {
       unoptimized: true,
-    },
-    trailingSlash: true,
+    }
     // Uncomment and set these to your actual base path and asset prefix if needed for GitHub Pages or other hosting
     // basePath: '/your-repo-name',
     // assetPrefix: '/your-repo-name/',
